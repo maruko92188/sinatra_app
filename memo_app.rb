be2aff00bad1fbc3a4aa48051fe9388c172631e3
maruko92188 <maruko.kazuki.f@gmail.com>
2026-04-25 22:47:51 +0900

    トップページを実装
    memo_appにトップページのルーテイングを追加
    共通レイアウト(index.erb)とトップページのレイアウト(index.erb)を追加

--- memo_app.rb
+++ memo_app.rb
@@ -5,3 +5,20 @@ require 'sinatra/reloader'
 require 'json'
 
 # メモアプリ本体
+
+JSON_FILE = './memo.json'
+
+get '/' do 
+  redirect '/memos'
+end
+
+get '/memos' do
+  memos =
+  if File.exist?(JSON_FILE)
+    JSON.parse(File.read(JSON_FILE))
+  else
+    {}
+  end
+  erb :index, locals: { memos: }
+end
+
