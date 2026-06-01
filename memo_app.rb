0cca988600e0a71daf462baf6e29abbe6377877b
maruko92188 <maruko.kazuki.f@gmail.com>
2026-06-01 19:58:21 +0900

    memo_app.rbでコメントアウトしていたidの型チェックを元に戻す。

--- memo_app.rb
+++ memo_app.rb
@@ -66,7 +66,7 @@ def load_memos
 end
 
 def find_memo(id)
-  # halt 404 unless id =~ /\A\d+\z/
+  halt 404 unless id =~ /\A\d+\z/
   settings.conn.exec_params('SELECT * FROM memos WHERE id = $1 LIMIT 1;', [id]).first || halt(404)
 end
 
